80fd84fd6d41a8fbcd63a5e55847fa59768a53ec
vikrm <abhishek8587singh@gmail.com>
2021-08-09 16:28:41 +0530

    added

---
+++ incl/footer.php
@@ -0,0 +1,15 @@
+<!-- footer component  -->
+<div>
+   <h1>footer</h1>
+</div>
+
+
+
+<!------------------------------------------- scripts  -------------------------------------------->
+<!----------- jquery  --------->
+<script src="https://code.jquery.com/jquery-3.6.0.slim.min.js"></script>
+<!---------- bootstrap  --------->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js"></script>
+
+<!----------- custom  ------------>
+<script src="/js/index.js"></script>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- title should be on top  -->
+<?php $title = 'index page'; ?>
+<?php include './incl/head.php'; ?>
+
+<body>
+   <!-- navbar  -->
+   <?php include './incl/nav.php'; ?>
+
+
+
+
+
+
+
+
+
+
+
+
+   <!--------- footer  ----------->
+   <?php include './incl/footer.php'; ?>
+   <script>
+   </script>
+</body>
+
+</html>
