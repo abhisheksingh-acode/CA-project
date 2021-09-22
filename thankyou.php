6642453ca2201adf853170c8ed8c2f32890d068e
vikrm <abhishek8587singh@gmail.com>
2021-09-22 10:58:18 +0530

    commit

---
+++ thankyou.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- title should be on top  -->
+<?php $title = 'ConsoLegal | Thank You'; ?>
+<?php include './incl/head.php'; ?>
+
+<body>
+   <!---------- navbar  ---------->
+   <?php include './incl/nav.php'; ?>
+
+   <section id="thankyou">
+      <div class="main-div mx-auto">
+         <span class="icon"><i class="fas fa-check"></i></span>
+         <h2 class="strong">Congratulations !</h2>
+         <h2 class="strong">Enquiry Received. Our Representative Will Contact
+            Shortly.</h2>
+         <p class="muted">We have Sent You Confirmation on Your Email Address. Please Check Your Email for Confirmation.</p>
+         <a href="/" class="btn an-btn">Go To Home</a>
+      </div>
+   </section>
+
+
+
+
+
+
+
+   <!--------- footer  ----------->
+   <?php include './incl/footer.php'; ?>
+</body>
+
+</html>
